d29f80d40a14a8dba53b28e8f33c48d6e050116e
Дмитрий Ошмарин <bndgrave1@gmail.com>
2022-06-17 16:52:31 +0500

    Commit 8

--- sp_13_task_1_broken_array_2.py
+++ sp_13_task_1_broken_array_2.py
@@ -1,27 +1,38 @@
+# def is_sorted(nums):
+#     if len(nums) > 1:
+#         return nums[0] < nums[-1]
+#     return True
+
 def broken_search(nums, target) -> int:
-    start = 0
-    end = len(nums) - 1
-    index = -1
-    while start <= end:
-        mid = (start + end) // 2
-        if nums[mid] == target:
-            index = mid
-            break
-        if nums[start] <= nums[mid]:
-            if nums[start] <= target <= nums[mid]:
-                end = mid - 1
-            else:
-                start = mid + 1
+    if len(nums) == 1:
+        if nums[0] == target:
+            return 0
         else:
-            if nums[mid] <= target <= nums[end]:
-                start = mid + 1
-            else:
-                end = mid -1
-    return index
+            return -1
+    # if len(nums) == 0:
+    #     return -1
+    mid = len(nums) // 2
+    if nums[mid] == target:
+        return mid
+    if nums[0] < nums[-1]: # is_sorted(nums[0:mid]):
+        if nums[0] <= target <= nums[mid-1]:
+            search_result = broken_search(nums[0:mid], target)
+            return search_result if search_result >= 0 else -1
+        else:
+            search_result = broken_search(nums[mid+1:len(nums)], target)
+            return search_result + mid + 1 if search_result >= 0 else -1
+    else:
+        if nums[mid+1] <= target <= nums[-1]:
+            search_result = broken_search(nums[mid+1:len(nums)], target)
+            return search_result + mid + 1 if search_result >= 0 else -1
+        else:
+            search_result = broken_search(nums[0:mid], target)
+            return search_result if search_result >= 0 else -1
 
 def test():
     arr = [19, 21, 100, 101, 1, 4, 5, 7, 12, 15]
     assert broken_search(arr, 5) == 6
+    # print(broken_search(arr, 1))
 
 
 # if __name__ == '__main__':
